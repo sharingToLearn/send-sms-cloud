1be936b73e929d164299d353b106f32fd9a1124b
Shahbaz Khan <shahbaz.khan1003@gmail.com>
2015-01-27 23:06:27 +0530

    resolved imports

--- src/main/java/com/shahbaz/rest/api/sendsms/controller/RestLayer.java
+++ src/main/java/com/shahbaz/rest/api/sendsms/controller/RestLayer.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shahbaz.rest.api.sendsms.domain.SMS;
